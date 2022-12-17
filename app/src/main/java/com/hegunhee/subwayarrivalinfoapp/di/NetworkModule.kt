0935d9ebf8a8fbcc9b22aa21b747bf3d5164548f
stephen.gh <stephen.gh@kakaocorp.com>
2022-12-17 20:44:08 +0900

    [gson-to-moshi] moshi 컨버터 생성

--- app/src/main/java/com/hegunhee/subwayarrivalinfoapp/di/NetworkModule.kt
+++ app/src/main/java/com/hegunhee/subwayarrivalinfoapp/di/NetworkModule.kt
@@ -5,24 +5,31 @@ import com.hegunhee.subwayarrivalinfoapp.network.SUBWAY_ARRIVAL_BASE_URL
 import com.hegunhee.subwayarrivalinfoapp.network.SUBWAY_INFO_BASE_URL
 import com.hegunhee.subwayarrivalinfoapp.network.SubwayArrivalApi
 import com.hegunhee.subwayarrivalinfoapp.network.SubwayInfoApi
+import com.squareup.moshi.Moshi
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.converter.moshi.MoshiConverterFactory
 import javax.inject.Singleton
 
 @InstallIn(SingletonComponent::class)
 @Module
 class NetworkModule {
 
+
+    @Singleton
+    @Provides
+    fun provideMoshi() : Moshi = Moshi.Builder().build()
+
     @Singleton
     @Provides
     fun provideSubwayInfoApi() : SubwayInfoApi{
         return Retrofit.Builder()
             .baseUrl(SUBWAY_INFO_BASE_URL)
-            .addConverterFactory(GsonConverterFactory.create())
+            .addConverterFactory(MoshiConverterFactory.create(provideMoshi()))
             .build()
             .create(SubwayInfoApi::class.java)
     }
@@ -32,7 +39,7 @@ class NetworkModule {
     fun provideSubwayArrivalApi() : SubwayArrivalApi{
         return Retrofit.Builder()
             .baseUrl(SUBWAY_ARRIVAL_BASE_URL)
-            .addConverterFactory(GsonConverterFactory.create())
+            .addConverterFactory(MoshiConverterFactory.create(provideMoshi()))
             .build()
             .create(SubwayArrivalApi::class.java)
     }
