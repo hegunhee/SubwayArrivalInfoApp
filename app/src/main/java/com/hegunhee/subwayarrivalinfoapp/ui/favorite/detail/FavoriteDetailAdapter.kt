5a27abeff3e606ef9abfa73d11fda01937f18242
hegunhee <gunhee0072@gmail.com>
2022-12-25 08:24:24 +0900

    refactor. 텍스트 설정을 xml에서 처리하도록 변경 + bindingAdapter 추가

--- app/src/main/java/com/hegunhee/subwayarrivalinfoapp/ui/favorite/detail/FavoriteDetailAdapter.kt
+++ app/src/main/java/com/hegunhee/subwayarrivalinfoapp/ui/favorite/detail/FavoriteDetailAdapter.kt
@@ -5,7 +5,6 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
-import com.hegunhee.subwayarrivalinfoapp.data.entity.Favorites
 import com.hegunhee.subwayarrivalinfoapp.data.json.subway_arrival.SubwayArrivalSmallDataWithStationLine
 import com.hegunhee.subwayarrivalinfoapp.databinding.ItemFavoriteDetailBinding
 
@@ -13,8 +12,7 @@ class FavoriteDetailAdapter() : ListAdapter<SubwayArrivalSmallDataWithStationLin
 
     inner class FavoriteDetailViewHolder(private val binding : ItemFavoriteDetailBinding) : RecyclerView.ViewHolder(binding.root){
         fun bind(subway : SubwayArrivalSmallDataWithStationLine) = with(binding){
-            time.text =  "${subway.time/60} 분 ${subway.time%60} 초"
-            message.text = subway.message
+            subwayArrivaldata = subway
         }
     }
 
