83d646db6e16ee0a8b1c4c55c696ecbd293215e1
hegunhee <gunhee0072@gmail.com>
2023-04-10 14:09:36 +0900

    refactor. Api를 Call 타입에서 coroutine을 사용해 원래 타입으로 가져옴

--- app/src/main/java/com/hegunhee/subwayarrivalinfoapp/network/SubwayArrivalApi.kt
+++ app/src/main/java/com/hegunhee/subwayarrivalinfoapp/network/SubwayArrivalApi.kt
@@ -1,18 +1,17 @@
 package com.hegunhee.subwayarrivalinfoapp.network
 
 import com.hegunhee.subwayarrivalinfoapp.data.json.subway_arrival.SubwayArrivalJson
-import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Path
 
 interface SubwayArrivalApi {
     @GET("{key}/{type}/{service}/{start_index}/{end_index}/{station}")
-    fun getSubwayInfo(
+    suspend fun getSubwayInfo(
         @Path("key") key : String,
         @Path("type") type : String = "json",
         @Path("service") service : String = "realtimeStationArrival",
         @Path("start_index") start_index : Int = 1,
         @Path("end_index") end_index : Int = 5,
         @Path("station") station_nm : String
-    ) : Call<SubwayArrivalJson>
+    ) : SubwayArrivalJson
 }
