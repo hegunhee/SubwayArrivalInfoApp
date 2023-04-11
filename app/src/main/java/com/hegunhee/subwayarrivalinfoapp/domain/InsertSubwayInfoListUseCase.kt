50456437770d08af8281110c2c1836d2fccc5a25
hegunhee <gunhee0072@gmail.com>
2023-04-11 16:29:14 +0900

    [지하철 단순 정보] InsertSubwayInfoListUseCase 코드 감소

--- app/src/main/java/com/hegunhee/subwayarrivalinfoapp/domain/InsertSubwayInfoListUseCase.kt
+++ app/src/main/java/com/hegunhee/subwayarrivalinfoapp/domain/InsertSubwayInfoListUseCase.kt
@@ -1,13 +1,9 @@
 package com.hegunhee.subwayarrivalinfoapp.domain
 
-import android.util.Log
-import com.hegunhee.subwayarrivalinfoapp.Util.subway_line_limit
-import com.hegunhee.subwayarrivalinfoapp.data.entity.SubwayInfoEntity
 import com.hegunhee.subwayarrivalinfoapp.model.Repository
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
-import retrofit2.awaitResponse
 import javax.inject.Inject
 
 @InstallIn(SingletonComponent::class)
@@ -16,16 +12,6 @@ class InsertSubwayInfoListUseCase @Inject constructor(
     private val repository: Repository
 ) : UseCase {
     suspend operator fun invoke(){
-        repository.getAllSubwayList().awaitResponse().body()?.let {
-            it.searchInfoBySubwayNameService.let {  service->
-                if(service.result.code == "INFO-000"){
-                    service.row.filter { it.line_num.substring(1) in subway_line_limit }.groupBy { it.station_nm }.map { subway ->
-                        SubwayInfoEntity(subway.key,subway.value.map{it.line_num.substring(1)})
-                    }.let {
-                        repository.insertSubwayInfoList(it)
-                    }
-                }
-            }
-        }
+        repository.insertAllSubwayList()
     }
 }
