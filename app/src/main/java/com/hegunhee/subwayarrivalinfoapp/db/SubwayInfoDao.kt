07c1ca20bbdbe10fa63fbff4defbeaeb90748792
hegunhee <gunhee0072@gmail.com>
2023-04-11 16:58:11 +0900

    BookMarked 수정 (Dao Replace -> Update), 함수 명 수정

--- app/src/main/java/com/hegunhee/subwayarrivalinfoapp/db/SubwayInfoDao.kt
+++ app/src/main/java/com/hegunhee/subwayarrivalinfoapp/db/SubwayInfoDao.kt
@@ -4,6 +4,7 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import androidx.room.Update
 import com.hegunhee.subwayarrivalinfoapp.data.entity.SubwayInfoEntity
 import kotlinx.coroutines.flow.Flow
 
@@ -13,8 +14,8 @@ interface SubwayInfoDao {
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun insertSubwayInfoList(subwayInfoList : List<SubwayInfoEntity>)
 
-    @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun toggleSubwayInfo(subwayInfoEntity: SubwayInfoEntity)
+    @Update
+    suspend fun toggleSubwayInfoBookMarked(subwayInfoEntity: SubwayInfoEntity)
 
     @Query("SELECT * FROM subwayInfoEntity ORDER BY isBookmarked DESC")
     fun getAllSubwayInfoByFlow() : Flow<List<SubwayInfoEntity>>
