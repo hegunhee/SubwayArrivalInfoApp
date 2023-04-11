32adb5c952f35fb72fa73cc90944e32d0c4eeba9
hegunhee <gunhee0072@gmail.com>
2023-04-11 15:53:52 +0900

    [지하철 단순 정보] 지하철 단순 정보 api를 Call방식에서 코루틴으로 변경

--- app/src/main/java/com/hegunhee/subwayarrivalinfoapp/network/SubwayInfoApi.kt
+++ app/src/main/java/com/hegunhee/subwayarrivalinfoapp/network/SubwayInfoApi.kt
@@ -1,5 +1,6 @@
 package com.hegunhee.subwayarrivalinfoapp.network
 
+import com.hegunhee.subwayarrivalinfoapp.BuildConfig
 import com.hegunhee.subwayarrivalinfoapp.data.json.subway_info.JsonSubwayInfo
 import retrofit2.Call
 import retrofit2.http.GET
@@ -7,11 +8,11 @@ import retrofit2.http.Path
 
 interface SubwayInfoApi {
     @GET("/{key}/{type}/{service}/{start_index}/{end_index}")
-    fun getSubwayInfo(
-        @Path("key") key : String,
+    suspend fun getSubwayInfo(
+        @Path("key") key : String = BuildConfig.SUBWAY_INFO_API_KEY,
         @Path("type") type : String = "json",
         @Path("service") service : String = "SearchInfoBySubwayNameService",
         @Path("start_index") start_index : Int = 1,
         @Path("end_index") end_inedx : Int = 768,
-    ) : Call<JsonSubwayInfo>
+    ) : JsonSubwayInfo
 }
