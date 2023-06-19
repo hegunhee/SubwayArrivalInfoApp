5e62da4c9fcccb15ea70bcc9638ff244c666ab57
hegunhee <gunhee0072@gmail.com>
2023-06-19 14:17:10 +0900

    [dataSource] feat : subwayInfoDao 관련 DataSource 코드 작성

--- app/src/main/java/com/hegunhee/subwayarrivalinfoapp/datasource/DefaultLocalDataSource.kt
+++ app/src/main/java/com/hegunhee/subwayarrivalinfoapp/datasource/DefaultLocalDataSource.kt
@@ -1,4 +1,26 @@
 package com.hegunhee.subwayarrivalinfoapp.datasource
 
-class DefaultLocalDataSource : LocalDataSource {
+import com.hegunhee.subwayarrivalinfoapp.data.entity.SubwayInfoEntity
+import com.hegunhee.subwayarrivalinfoapp.db.SubwayInfoDao
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class DefaultLocalDataSource @Inject constructor(
+    private val subwayInfoDao : SubwayInfoDao
+) : LocalDataSource {
+    override suspend fun insertSubwayInfoList(infoList : List<SubwayInfoEntity>) {
+       subwayInfoDao.insertSubwayInfoList(infoList)
+    }
+
+    override suspend fun updateSubwayInfoBookMark(subwayInfoEntity: SubwayInfoEntity) {
+        subwayInfoDao.toggleSubwayInfoBookMarked(subwayInfoEntity)
+    }
+
+    override fun getAllSubwayInfoListByFlow(): Flow<List<SubwayInfoEntity>> {
+        return subwayInfoDao.getAllSubwayInfoByFlow()
+    }
+
+    override suspend fun getSubwayInfoByNameOrNull(stationName: String): SubwayInfoEntity? {
+        return subwayInfoDao.getSubwayInfoByName(stationName)
+    }
 }
--- app/src/main/java/com/hegunhee/subwayarrivalinfoapp/datasource/LocalDataSource.kt
+++ app/src/main/java/com/hegunhee/subwayarrivalinfoapp/datasource/LocalDataSource.kt
@@ -1,4 +1,18 @@
 package com.hegunhee.subwayarrivalinfoapp.datasource
 
+import com.hegunhee.subwayarrivalinfoapp.data.entity.SubwayInfoEntity
+import kotlinx.coroutines.flow.Flow
+
 interface LocalDataSource {
+
+    suspend fun insertSubwayInfoList(infoList : List<SubwayInfoEntity>)
+
+    suspend fun updateSubwayInfoBookMark(subwayInfoEntity: SubwayInfoEntity)
+
+    fun getAllSubwayInfoListByFlow() : Flow<List<SubwayInfoEntity>>
+
+    suspend fun getSubwayInfoByNameOrNull(stationName : String) : SubwayInfoEntity?
+
+
+
 }
