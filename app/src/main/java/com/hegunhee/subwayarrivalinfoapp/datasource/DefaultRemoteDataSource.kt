ec40eb65b7e45b85a99c202981255461e50e5075
hegunhee <gunhee0072@gmail.com>
2023-06-19 14:49:30 +0900

    [dataSource] feat : subwayApi 관련 RemoteDataSource 코드 작성

--- app/src/main/java/com/hegunhee/subwayarrivalinfoapp/datasource/DefaultRemoteDataSource.kt
+++ app/src/main/java/com/hegunhee/subwayarrivalinfoapp/datasource/DefaultRemoteDataSource.kt
@@ -1,5 +1,13 @@
 package com.hegunhee.subwayarrivalinfoapp.datasource
 
+import com.hegunhee.subwayarrivalinfoapp.data.json.subway_info.JsonSubwayInfo
+import com.hegunhee.subwayarrivalinfoapp.network.SubwayInfoApi
+import javax.inject.Inject
+
 class DefaultRemoteDataSource @Inject constructor(
+    private val subwayInfoApi: SubwayInfoApi
 ) : RemoteDataSource {
+    override suspend fun getAllSubwayList(): JsonSubwayInfo {
+        return subwayInfoApi.getSubwayInfo()
+    }
 }
