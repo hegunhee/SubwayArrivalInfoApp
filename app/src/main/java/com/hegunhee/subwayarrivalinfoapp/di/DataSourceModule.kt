b9a9b7fa2dec2340f68106ae637873dc8ac27e9c
hegunhee <gunhee0072@gmail.com>
2023-06-19 14:53:38 +0900

    [dataSource] feat : RemoteDataSource 객체 의존성 주입하는 Module 생성

--- app/src/main/java/com/hegunhee/subwayarrivalinfoapp/di/DataSourceModule.kt
+++ app/src/main/java/com/hegunhee/subwayarrivalinfoapp/di/DataSourceModule.kt
@@ -1,7 +1,9 @@
 package com.hegunhee.subwayarrivalinfoapp.di
 
 import com.hegunhee.subwayarrivalinfoapp.datasource.DefaultLocalDataSource
+import com.hegunhee.subwayarrivalinfoapp.datasource.DefaultRemoteDataSource
 import com.hegunhee.subwayarrivalinfoapp.datasource.LocalDataSource
+import com.hegunhee.subwayarrivalinfoapp.datasource.RemoteDataSource
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
@@ -15,4 +17,8 @@ abstract class DataSourceModule {
     @Binds
     @Singleton
     abstract fun provideLocalDataSource(defaultLocalDataSource: DefaultLocalDataSource) : LocalDataSource
+
+    @Binds
+    @Singleton
+    abstract fun provideRemoteDataSource(defaultRemoteDataSource: DefaultRemoteDataSource) : RemoteDataSource
 }
