934de9c5d9043a512c20f7351013d07d68eeffaa
hegunhee <gunhee0072@gmail.com>
2023-06-19 14:49:30 +0900

    [dataSource] feat : subwayApi 관련 RemoteDataSource 코드 작성

--- app/src/main/java/com/hegunhee/subwayarrivalinfoapp/datasource/RemoteDataSource.kt
+++ app/src/main/java/com/hegunhee/subwayarrivalinfoapp/datasource/RemoteDataSource.kt
@@ -1,4 +1,10 @@
 package com.hegunhee.subwayarrivalinfoapp.datasource
 
+import com.hegunhee.subwayarrivalinfoapp.data.json.subway_info.JsonSubwayInfo
+
 interface RemoteDataSource {
+
+    suspend fun getAllSubwayList() : JsonSubwayInfo
+
+
 }
