325da33b807a6bb085e037f2040b26055c8996c7
hegunhee <gunhee0072@gmail.com>
2023-06-20 09:40:58 +0900

    [refactor-usecase] style : 파라미터를 스네이크 케이스에서 카멜 케이스로 변경

--- app/src/main/java/com/hegunhee/subwayarrivalinfoapp/domain/DeleteFavoritesUseCase.kt
+++ app/src/main/java/com/hegunhee/subwayarrivalinfoapp/domain/DeleteFavoritesUseCase.kt
@@ -8,11 +8,8 @@ import javax.inject.Inject
 
 @InstallIn(SingletonComponent::class)
 @Module
-class DeleteFavoritesUseCase @Inject constructor(
-    private val repository: Repository
-): UseCase {
-
-    suspend operator fun invoke(station_info : String){
-        repository.deleteFavorite(station_info)
+class DeleteFavoritesUseCase @Inject constructor(private val repository: Repository): UseCase {
+    suspend operator fun invoke(stationInfo : String){
+        repository.deleteFavorite(stationInfo)
     }
 }
