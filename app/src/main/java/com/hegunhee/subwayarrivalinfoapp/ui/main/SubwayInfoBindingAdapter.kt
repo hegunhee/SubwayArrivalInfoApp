1e33c7c1738b4967be15779f44d74bb9e3417186
hegunhee <gunhee0072@gmail.com>
2023-06-22 17:43:34 +0900

    refactor : 사용 안하는 Chip 코드 주석 삭제

--- app/src/main/java/com/hegunhee/subwayarrivalinfoapp/ui/main/SubwayInfoBindingAdapter.kt
+++ app/src/main/java/com/hegunhee/subwayarrivalinfoapp/ui/main/SubwayInfoBindingAdapter.kt
@@ -2,25 +2,6 @@ package com.hegunhee.subwayarrivalinfoapp.ui.main
 
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
-import com.google.android.material.chip.Chip
-import com.google.android.material.chip.ChipGroup
-import com.hegunhee.subwayarrivalinfoapp.Util.SubwayLineColor
-import com.hegunhee.subwayarrivalinfoapp.Util.setColor
-
-//@BindingAdapter("setSubwayLines")
-//fun ChipGroup.setChips(subwayLine : List<String>?){
-//    removeAllViews()
-//    subwayLine?.forEach { line ->
-//        addView(Chip(context).apply {
-//            text = line
-//            for(subwayColor in SubwayLineColor.values()){
-//                if(subwayColor.line == line){
-//                    setColor(subwayColor.getColor())
-//                }
-//            }
-//        })
-//    }
-//}
 
 @BindingAdapter("setSubwayLines")
 fun RecyclerView.setSubwayLines(subwayLine: List<String>?) {
